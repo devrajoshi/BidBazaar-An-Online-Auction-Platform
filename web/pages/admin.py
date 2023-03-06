d5683fa50c92e32c151c035ab13c34f47bef0c05
iambigyandahal <iambigyandahal@gmail.com>
2023-03-06 19:25:25 +0545

    `fix`: fixed and updated previous codes to make it work correctly

--- web/pages/admin.py
+++ web/pages/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import Item, User
+from .models import Item, User, Bid
 
 # Register your models here.
-admin.site.register(Item)
-admin.site.register(User)
+admin.site.register([Item, User, Bid])
--- web/pages/models.py
+++ web/pages/models.py
@@ -1,23 +1,5 @@
-from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User as DjangoUser
-
-
-# Create your models here.
-class Item(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    seller = models.CharField(max_length=255)
-    seller_id = models.PositiveBigIntegerField(default=0)
-    image = models.ImageField(default=None)
-    added_at = models.DateTimeField(auto_now_add=True)
-    deadline_at = models.DateTimeField(blank=True)
-    slug = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.title
-
 
 class User(models.Model):
     user = models.OneToOneField(
@@ -27,6 +9,7 @@ class User(models.Model):
     email_token = models.CharField(max_length=255, null=True, blank=True)
     avatar = models.CharField(max_length=255, blank=True, null=True)
     last_modified = models.DateTimeField(null=True, blank=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
     pan_no = models.PositiveIntegerField()
     citizen_no = models.PositiveIntegerField()
 
@@ -35,3 +18,24 @@ class User(models.Model):
 
     def __str__(self):
         return self.get_full_name()
+
+class Item(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(default=None)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    seller = models.ForeignKey(User, on_delete=models.PROTECT)
+    added_at = models.DateTimeField(auto_now_add=True)
+    deadline_at = models.DateTimeField(blank=True)
+    slug = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+class Bid(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    bid_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.item
